3d7ff8439b6460419bb09f721ac5c52d2912ca8c
nhorro <nhorro@gmail.com>
2021-06-30 20:33:58 +0000

    naming!

--- dockers/darknet-gpu/build.sh
+++ dockers/darknet-gpu/build.sh
@@ -1,1 +1,1 @@
-docker build -t darknet-gpu --build-arg http_proxy=$http_proxy --build-arg https_proxy=$http_proxy .
+docker build -t nhorro/darknet-gpu --build-arg http_proxy=$http_proxy --build-arg https_proxy=$http_proxy .
