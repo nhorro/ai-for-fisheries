25cf3778a22fe978596c37ddb16ac4f8bbde2120
nhorro <nhorro@gmail.com>
2021-08-04 19:40:53 +0000

    cambia sh e instrucciones para entrenar detached

--- data/datasets/kaggle-fisheries-yolo/train-tiny.sh
+++ data/datasets/kaggle-fisheries-yolo/train-tiny.sh
@@ -1,3 +1,3 @@
 echo $CONDA_PREFIX
 MODEL_NAME=kaggle-fisheries
-darknet detector train $MODEL_NAME.data $MODEL_NAME-yolo4tiny.cfg yolov4-tiny.conv.29 -json_port 8070 -mjpeg_port 8090 -ext_output -dont_show -map | tee $MODEL_NAME-training-log.txt
+darknet detector train $MODEL_NAME.data $MODEL_NAME-yolo4tiny.cfg yolov4-tiny.conv.29 -json_port 8070 -mjpeg_port 8090 -ext_output -dont_show -map > ./logs/darknet_logs.log 2>&1 &
